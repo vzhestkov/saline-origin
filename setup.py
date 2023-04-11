07d81c7fec96687c0e0b1a21aa0cb657f67a09ea
Victor Zhestkov <Victor.Zhestkov@suse.com>
2023-04-12 01:58:31 +0300

    Put missing saline.data and saline.config to setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from distutils.core import setup
 
 setup(
     name="saline",
+    url="https://github.com/vzhestkov/saline",
     description="The salt event collector and manager",
     author="Victor Zhestkov",
     author_email="vzhestkov@gmail.com",
     version="2023.04.11",
-    packages=["saline"],
+    packages=["saline", "saline.config", "saline.data"],
     license="GPL-2.0",
     scripts=["salined"],
 )
