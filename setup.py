df9afcc1cb8773f1fef533de01aab551c734599f
vzhestkov <vzhestkov@suse.com>
2024-09-30 16:52:05 +0200

    Calculate saline version in setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
+import os
+import sys
+
 from setuptools import find_packages, setup
+
+
+def get_version():
+    cwd = os.getcwd()
+    print("DEBUGIT %s DBG" % (cwd), file=sys.stderr)
+    return "2024.09.30"
 
 
 setup(
     name="saline",
-    url="https://github.com/vzhestkov/saline",
+    url="https://github.com/openSUSE/saline",
     description="The salt event collector and manager",
     author="Victor Zhestkov",
     author_email="vzhestkov@gmail.com",
-    version="2024.09.30",
+    version=get_version(),
     packages=find_packages(),
     license="GPL-2.0",
     scripts=["salined"],
