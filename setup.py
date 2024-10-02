abd372b77cd6602e1487a6013bf793fcab99aa3a
vzhestkov <vzhestkov@suse.com>
2024-10-02 11:55:52 +0200

    Cleanup saline version calculation

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import packaging.version
 import re
 import subprocess
 import sys
@@ -6,22 +7,20 @@ import sys
 from setuptools import find_packages, setup
 
 
-def get_version():
+def get_saline_version():
     cwd = os.getcwd()
     m = re.search(r"salineX-(\d{4}\.\d{2}.\d{2})", cwd)
     if m:
-        print("XXX %s XXX" % m[1], file=sys.stderr)
-        #return m[1]
+        return str(packaging.version.Version(m[1]))
     file_date = subprocess.check_output(
         "find . -type f -printf '%AY.%Am.%Ad\n' | sort -r | head -n 1",
         shell=True,
     )
     file_date = file_date.decode()[:-1]
     if file_date:
-        print("ZZZ %s ZZZ" % file_date, file=sys.stderr)
-        return file_date
+        return str(packaging.version.Version(file_date))
     # Fallback to some default value if not possible to calculate
-    return "2024.01.16"
+    return "2024.1.16"
 
 
 setup(
@@ -30,7 +29,7 @@ setup(
     description="The salt event collector and manager",
     author="Victor Zhestkov",
     author_email="vzhestkov@gmail.com",
-    version=get_version(),
+    version=get_saline_version(),
     packages=find_packages(),
     license="GPL-2.0",
     scripts=["salined"],
